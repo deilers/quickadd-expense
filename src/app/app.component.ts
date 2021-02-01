c59f6fb091ed83db9365b3c6886bce5a0921ab8f
Devon Eilers <devon.d.eilers@gmail.com>
2021-02-01 12:50:26 -0800

    add basic form input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  constructor() {}
+
+  form: FormGroup;
+
   title = 'quickadd-expenses';
+
+  ngOnInit(): void {
+    this.form = new FormGroup({});
+  }
 }
